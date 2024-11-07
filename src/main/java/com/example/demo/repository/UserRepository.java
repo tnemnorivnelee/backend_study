391d95820a0c8f1b07a68cf331365049b7311940
tnemnorivnelee <tnemnorivnelee@gmail.com>
2024-11-07 16:07:00 +0900

    .

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByUsername(String username);
 
