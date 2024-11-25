38f1075859fea5978e7c5ad6d0c8b51405de41ff
tnemn <tnemnorivnelee@gmail.com>
2024-11-25 23:36:58 +0900

    ADD: swagger 간단 적용

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.dto.userDto.UserRequestDTO;
 import com.example.demo.dto.userDto.UpdateUserPasswordRequest;
 import com.example.demo.service.impl.UserServiceImpl;
 import com.example.demo.service.inter.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name="User Controller", description = "User 컨트롤러 부분")
 public class UserController {
 
     private final UserService userService;
@@ -50,6 +53,7 @@ public class UserController {
 
     // Update pwd
     @PutMapping("/update")
+    @Operation(summary = "update user password", description = "user 비밀번호를 변경합니다.")
     public ResponseEntity<String> updateUserPassword(@RequestBody UpdateUserPasswordRequest request) {
         userService.updatePassword(request);
 
